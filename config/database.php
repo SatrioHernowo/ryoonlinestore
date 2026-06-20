2d26dd7f53f212ef32f7bdd1e6d2ac0410948241
SatrioHernowo <owogeming@gmail.com>
2026-06-21 03:11:00 +0700

    Fix Pdo SSL constant deprecation for php 8.5

--- config/database.php
+++ config/database.php
@@ -59,9 +59,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-    PDO::MYSQL_ATTR_SSL_CA => base_path(env('MYSQL_ATTR_SSL_CA')),
-]) : [],
+            'options' => extension_loaded('pdo_mysql') ? [
+    class_exists('Pdo\Mysql') ? \Pdo\Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA => base_path(env('MYSQL_ATTR_SSL_CA')),
+    class_exists('Pdo\Mysql') ? \Pdo\Mysql::ATTR_SSL_VERIFY_SERVER_CERT : PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+] : [],
         ],
 
         'mariadb' => [
