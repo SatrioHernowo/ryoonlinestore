ea48fb2823452ed16054d818d6f20f1ca01bf313
SatrioHernowo <owogeming@gmail.com>
2026-06-22 00:37:49 +0700

    alhamdulillah

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
         $viewData["title"] = "About us - Online Store";
         $viewData["subtitle"] = "About us";
         $viewData["description"] = "This is an about page ...";
-        $viewData["author"] = "Developed by: Your Name";
+        $viewData["author"] = "Developed by: Satrio Hernowo";
 
         return view('home.about')->with("viewData", $viewData);
     }
